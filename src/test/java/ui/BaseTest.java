dfa0b93b6cdfe9040a0982cba0b3786cfdfb30dc
gchursov <topsycreed@gmail.com>
2025-04-16 19:31:19 +0200

    Remove unused imports

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -5,8 +5,6 @@ import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
