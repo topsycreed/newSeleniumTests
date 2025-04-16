700e026fd516e29b86af18d4e23af284d5c525d2
gchursov <topsycreed@gmail.com>
2025-04-16 16:20:47 +0200

    Add factory method to init web driver

--- src/test/java/ui/BaseTest.java
+++ src/test/java/ui/BaseTest.java
@@ -1,21 +1,26 @@
 package ui;
 
+import chursov.configs.TestPropertiesConfig;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.time.Duration;
 
+import static chursov.patterns.WebDriverFactory.createWebDriver;
+
 class BaseTest {
     WebDriver driver;
+    TestPropertiesConfig configProperties = ConfigFactory.create(TestPropertiesConfig.class, System.getProperties());
     protected static final String BASE_URL = "https://bonigarcia.dev/selenium-webdriver-java/";
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        driver = createWebDriver(configProperties.browser());
         driver.get(BASE_URL);
-        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
     }
 
